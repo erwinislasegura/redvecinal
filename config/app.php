13a9c83a3baf02919422b6b7c4277c119a872cef
erwinislasegura <erwin.2785@gmail.com>
2026-08-16 14:09:53 -0400

    Mejorar interfaz administrativa

--- config/app.php
+++ config/app.php
@@ -2,10 +2,11 @@
 declare(strict_types=1);
 
 return [
-    'host' => 'localhost',
-    'port' => '3306',
-    'database' => 'redvecinal',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8mb4',
+    'name' => 'RedVecinal',
+    'base_url' => '',
+    'timezone' => 'America/Santiago',
+    'locale' => 'es_CL',
+    'debug' => false,
+    'uploads_max_mb' => 8,
+    'offline_queue' => true,
 ];
