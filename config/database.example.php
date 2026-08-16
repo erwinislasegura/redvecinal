bebfa1d93ebebdd71a8822e23ed5020ebd5a273f
erwinislasegura <erwin.2785@gmail.com>
2026-08-16 14:10:01 -0400

    Restaurar ejemplo de base de datos

---
+++ config/database.example.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'host' => 'localhost',
+    'port' => '3306',
+    'database' => 'redvecinal',
+    'username' => 'usuario_mysql',
+    'password' => '',
+    'charset' => 'utf8mb4',
+];
