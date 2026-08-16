b21989321237afcf1952c689be6bcd77a99971fd
desarrollovolcan <eisla@fvolcan.cl>
2026-08-16 13:55:39 -0400

    Data

--- config/app.php
+++ config/app.php
@@ -2,12 +2,10 @@
 declare(strict_types=1);
 
 return [
-    'name' => 'RedVecinal',
-    'base_url' => '',
-    'timezone' => 'America/Santiago',
-    'locale' => 'es_CL',
-    'debug' => false,
-    'uploads_max_mb' => 8,
-    'offline_queue' => true,
+    'host' => 'localhost',
+    'port' => '3306',
+    'database' => 'redvecinal',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8mb4',
 ];
-
--- config/database.example.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'host' => 'localhost',
-    'port' => '3306',
-    'database' => 'redvecinal',
-    'username' => 'usuario_mysql',
-    'password' => '',
-    'charset' => 'utf8mb4',
-];
