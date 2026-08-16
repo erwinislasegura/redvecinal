c667b63e0f0db68f30fde5c9d8d4aade298c4fb7
erwinislasegura <erwin.2785@gmail.com>
2026-08-16 14:09:47 -0400

    Mejorar interfaz administrativa

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'redvecinal-v1';
-const STATIC = ['./public/assets/css/bootstrap.min.css','./public/assets/css/app.css','./public/assets/js/bootstrap.bundle.min.js','./public/assets/js/app.js','./public/assets/img/icon.svg','./','./ingresar'];
+const CACHE = 'redvecinal-v2';
+const STATIC = ['./public/assets/css/bootstrap.min.css','./public/assets/css/app.css','./public/assets/css/admin.css','./public/assets/js/bootstrap.bundle.min.js','./public/assets/js/app.js','./public/assets/img/icon.svg','./','./ingresar'];
 self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(STATIC)).catch(() => {})));
 self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
 self.addEventListener('fetch', (event) => {
